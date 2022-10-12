d42d55d816551d595fba1333510127deee59f96a
Elvis Liberato <liberatov13@gmail.com>
2022-10-12 20:03:07 -0300

    Initial commit

---
+++ src/test/java/br/com/liberato/fintech/FintechApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.liberato.fintech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
