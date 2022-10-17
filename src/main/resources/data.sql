10dd38a0761ad1fb04ef9a3486400b50834aad17
Elvis Liberato <liberatov13@gmail.com>
2022-10-17 19:14:44 -0300

    Atualizando contas pré-cadastradas no H2

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
-insert into conta values ('123456', 'Elvis', 20000)
+insert into conta values ('123456', 'Roberto', 20000)
+insert into conta values ('000123', 'Lucas', 150)
 insert into conta values ('654321', 'João', 15000)
 insert into conta values ('789123', 'Jose', 10000)
 
