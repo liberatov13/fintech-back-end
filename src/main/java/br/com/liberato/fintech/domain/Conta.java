70931604f58066dfcb98af672c2df447ccd264e3
Elvis Liberato <liberatov13@gmail.com>
2022-10-17 01:49:10 -0300

    Organizando imports

--- src/main/java/br/com/liberato/fintech/domain/Conta.java
+++ src/main/java/br/com/liberato/fintech/domain/Conta.java
@@ -1,11 +1,12 @@
 package br.com.liberato.fintech.domain;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Getter
 @Setter
